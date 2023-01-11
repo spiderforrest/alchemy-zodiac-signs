694d63a884b82f556b0e0e0915bbe5a5eac3a848
spiderforrest <spider@spood.org>
2023-01-10 17:29:14 -0800

    oh okay that's all i needed for images rendering anyway they do now

--- src/components/ZodiacCard/ZodiacCard.js
+++ src/components/ZodiacCard/ZodiacCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ZodiacCard(arg) {
   return (
     <div className="zodiac" id={arg.key}>
-      <img className="zodiac" src={`${arg.name}.png`}></img>
+      <img className="zodiac" src={`./images/${arg.name}.png`}></img>
       <h3 className="zodiac">{arg.symbol}</h3>
       <p className="zodiac">{arg.dates}</p>
     </div>
